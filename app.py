89774ac1d9bf7f927a252328a292d08695a06a36
AnastasiaS25 <anlsob@yandex.ru>
2025-08-13 15:35:18 +0300

    закончили

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #
+'''
 import my_module
 
 print ("hello world")
@@ -11,3 +12,14 @@ if __name__ == '__main__':
     print('самостоятельный запуск')
 else:
     print(__name__, 'был импортирован')
+    '''
+import tkinter as tk
+from my_module import study_entry
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Изучение элементов GUI")
+    root.geometry("400x300")
+    tk.Button(root, text="Изучение полей ввода", command = study_entry).pack(pady=20)  
+    
+    root.mainloop()    
--- my_module.py
+++ my_module.py
@@ -1,12 +1,12 @@
 #
-
+'''
 pi = 3.14
 
 def summa(a:int, b:float) -> int:
-    '''
+    
     эта функция 
     делает a + b
-    '''
+    
     return a + b
 
 
@@ -15,3 +15,27 @@ if __name__ == '__main__':
     print('самостоятельный запуск')
 else:
     print(__name__, 'был импортирован')
+'''
+import tkinter as tk
+from tkinter import messagebox
+
+
+        
+def study_entry():
+    window = tk.Toplevel()
+    window.title("Поля ввода")
+    window.geometry("300x200")
+    
+    tk.Label(window, text="Имя:").pack(pady=5)
+    entry1 = tk.Entry(window)
+    entry1.pack(pady=5)
+  
+    tk.Label(window, text="Пароль:").pack(pady=5)
+    entry2 = tk.Entry(window, show="*")
+    entry2.pack(pady=5)
+    def show_values():
+        messagebox.showinfo("Результат", f"Имя: {entry1.get()}\nПароль: {entry2.get()}")
+    tk.Button(window, text="Показать", command=show_values).pack(pady=10)
+    
+ 
+    
