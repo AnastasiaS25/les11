277cd5c0684eb9c29d0ef1980f9f029f9aa67c93
AnastasiaS25 <anlsob@yandex.ru>
2025-08-13 15:19:40 +0300

    изучили точку входа

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ print ("hello world")
 
 x = my_module.summa(1.6, 2)
 
-print(x)
+#print(x)
+if __name__ == '__main__':
+    print('самостоятельный запуск')
+else:
+    print(__name__, 'был импортирован')
