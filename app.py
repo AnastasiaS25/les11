2ad476a27a17c86a867186fe38aac8a38c7fab3e
AnastasiaS25 <anlsob@yandex.ru>
2025-08-13 15:08:20 +0300

    1508

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 #
-print('ura')
+import my_module
+
+print ("hello world")
+
+
+x = my_module.summa(1.6, 2)
+
+print(x)
--- my module.py
+++ my module.py
@@ -1,2 +1,13 @@
 #
-print('ura?')
+
+pi = 3.14
+
+def summa(a:int, b:float) -> int:
+    '''
+    эта функция 
+    делает a + b
+    '''
+    return a + b
+
+
+print("привет из модуля")
